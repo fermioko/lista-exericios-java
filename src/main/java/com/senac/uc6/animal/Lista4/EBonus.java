e891a562a08c17c2c62ba8b1f87d622c82929621
fernanda.mhmitsuhara <mioko.senac.2023@gmail.com>
2024-11-19 19:35:39 -0300

    2024-11-13-aula exercicios

--- src/main/java/com/senac/uc6/animal/Lista4/EBonus.java
+++ src/main/java/com/senac/uc6/animal/Lista4/EBonus.java
@@ -5,7 +5,7 @@ package com.senac.uc6.animal.Lista4;
 public class EBonus {
     public static void main(String[] args) {
 
-        int[] numeros = {2,5,9,3,4,1};
+        //int[] numeros = {2,5,9,3,4,1};
 
         
     }
